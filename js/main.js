08e9bcb87c8acc77b5b30e1183e46249123bbb25
Dakota O'Neil <oneilkody@gmail.com>
2021-08-03 18:16:19 -0500

    complete functionality and add styling to make it better

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.querySelector('#doTheThing').addEventListener('click', flipTheName)
+document.querySelector('#home').addEventListener('click', homeRefresh)
 
 async function flipTheName() {
     const firstName = document.querySelector('#firstName').value 
@@ -19,5 +20,13 @@ async function flipTheName() {
 
     let flippedName = `${lastInit}${firstLeftover} ${firstInit}${lastLeftover}`
 
-    document.getElementById('flippedName').innerText = flippedName
+    document.getElementById('introToName').innerText = `Your flipped name is:`
+    document.getElementById('flippedName').innerText = `${flippedName}`
+
+    document.getElementById('introToName').classList.remove('hidden')
+    document.getElementById('flippedName').classList.remove('hidden')
+}
+
+async function homeRefresh() {
+    location.reload()
 }
